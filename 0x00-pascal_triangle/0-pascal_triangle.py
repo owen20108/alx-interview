27664da904fd01ce9fc25aec8cb88556ac1fd301
owen20108 <owen20108@yahoo.com>
2024-04-26 00:28:51 +0000

    tri

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,45 @@
-def pascal_triangle(n):
-    if n <= 0:
-        return []  # Return an empty list if n is less than or equal to 0
-    
-    triangle = []  # Initialize the list to store the rows of Pascal's triangle
-    
-    for i in range(n):
-        row = []  # Initialize a list for the current row
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                row.append(1)  # The first and last elements in each row are 1
-            else:
-                # Other elements are the sum of the two elements directly above in the previous row
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(row)  # Add the current row to the triangle list
-    
-    return triangle
-
-# Test the function using the provided main script
-def print_triangle(triangle):
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+# Recusrive method to create the series
+def computePascal(col, row):
+    # There are three things to compute
+    # 1. Left edge: col is 0
+    # 2. Right edge: col is same as row
+    if col == row or col == 0:
+        return 1
+    # 3. any other cell: col-1 + col of the previous row
+    else:
+        return computePascal(col - 1, row - 1) + computePascal(col, row - 1)
 
 
+# Method to create the triangle for `N` row
+def printTriangle(num):
+    for r in range(num):
+        # upon observation, we can deduce the relation
+        # num_cols = num_rows + 1
+        for c in range(r + 1):
+            print(str(computePascal(c, r)), end=" ")
+        print("\n")
 
+
+printTriangle(10)
+"""
+Output:
+1 
+
+1 1 
+
+1 2 1 
+
+1 3 3 1 
+
+1 4 6 4 1 
+
+1 5 10 10 5 1 
+
+1 6 15 20 15 6 1 
+
+1 7 21 35 35 21 7 1 
+
+1 8 28 56 70 56 28 8 1 
+
+1 9 36 84 126 126 84 36 9 1 
+"""
