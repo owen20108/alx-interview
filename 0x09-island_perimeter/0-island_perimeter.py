289526cc1d7ebfb3736ef328975ae3d7603f7ec4
owen20108 <owen20108@yahoo.com>
2024-06-25 05:32:52 +0000

    py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,55 +1,26 @@
-
 #!/usr/bin/python3
-"""Island Perimeter - ALX Interview"""
-
-
-def check_val(x):
-    """_summary_
-
-    Args:
-        n (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    if (x == 0):
-        return 1
-    return 0
+"""Defines island perimeter finding function."""
 
 
 def island_perimeter(grid):
-    """_summary_
-
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
     Args:
-        grid (_type_): _description_
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
     """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "length must be between 1 an 100"
-
-    x = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                "grid numbers must be 0 or 1"
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                if i-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i-1][j])
-                if j-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i][j-1])
-
-                try:
-                    x += check_val(grid[i+1][j])
-                except IndexError:
-                    x += 1
-                try:
-                    x += check_val(grid[i][j+1])
-                except IndexError:
-                    x += 1
-
-    return x
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
