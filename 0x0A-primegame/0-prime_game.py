40e2417719f0c7e2daacb095cfacd287583a1a9d
owen20108 <owen20108@yahoo.com>
2024-07-04 02:52:21 +0000

    o

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,7 @@
 Prime Game
 '''
 
+
 def get_primes(nums):
     '''
     Get the count of primes up to each number in nums
@@ -10,25 +11,26 @@ def get_primes(nums):
     n = max(nums)
     if n < 1:
         return None
-    
+
     # Sieve of Eratosthenes
     prime = [True] * (n + 1)
     prime[0], prime[1] = False, False  # 0 and 1 are not prime numbers
     p = 2
-    
+
     while (p * p <= n):
         if prime[p]:
             for j in range(p * p, n + 1, p):
                 prime[j] = False
         p += 1
-    
+
     # Create a list of prime counts up to each number in nums
     prime_cnt = []
     for num in nums:
         cnt = sum(prime[:num + 1])
         prime_cnt.append(cnt)
-    
+
     return prime_cnt
+
 
 def isWinner(x, nums):
     '''
@@ -36,20 +38,21 @@ def isWinner(x, nums):
     '''
     if x <= 0 or nums is None or min(nums) < 0:
         return None
-    
+
     prime_cnt = get_primes(nums)
     if prime_cnt is None:
         return None
-    
+
     # Count the number of rounds won by Maria
     maria_wins = sum(1 for cnt in prime_cnt if cnt % 2 == 1)
-    
+
     if maria_wins > x / 2:
         return "Maria"
     elif maria_wins < x / 2:
         return "Ben"
     else:
         return None
+
 
 # Example usage
 if __name__ == "__main__":
